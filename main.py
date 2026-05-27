985eecb9bc682e04ae8084511add6fc3a4873db8
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 03:12:25 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterVetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterVetchWorker()
+    print("Worker engine initialized.")
